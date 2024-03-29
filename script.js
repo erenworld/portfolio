aeda4aab8456a1840fff7e8b071115600f6bf4f5
Eren Turkoglu <144266052+erenworld@users.noreply.github.com>
2024-03-29 22:26:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -92,3 +92,30 @@ document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
 wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 wrapper.addEventListener("mouseleave", autoPlay);
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+
+// Écrivez le code pour additionner tous les salaires et les enregistrer dans la variable sum. Devrait être égale à 390 dans l’exemple ci-dessus.
+
+// Si salaries est vide, le résultat doit être 0.
+// let sum = 0;
+// for (let key in salaries) {
+//   sum += salaries[keys];
+// }
+// alert(sum);
+
+////////////////////
+// Créez une fonction multiplyNumeric(obj) qui multiplie
+// toutes les valeurs de propriétés numériques de obj par 2.
+//
+// function multiplyNumeric(obj) {
+//   for (let key in obj) {
+//     if (typeof obj[key] == "number") {
+//       obj[key] *= 2;
+//     }
+//   }
+// }
